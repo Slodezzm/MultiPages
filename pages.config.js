6e76e15f303c5737a2c5a73dc5da9af459b7349f
Slode <191782262@qq.com>
2020-11-17 09:51:38 +0800

    Update pages.config.js

--- pages.config.js
+++ pages.config.js
@@ -2,7 +2,7 @@ module.exports = {
   'PC/bobing': {
     template: 'public/pc.html',
     filename: 'bobing-PC.html',
-    title: '王者直播 - 中秋博饼',
+    title: '',
     cdn:{
       js: ['vue','vuex','vue-router','axios','element-ui','jquery','socket.io'],
       css: ['element-ui']
@@ -11,7 +11,7 @@ module.exports = {
   'H5/bobing': {
     template: 'public/h5.html',
     filename: 'bobing-H5.html',
-    title: '中秋博饼',
+    title: '',
     cdn:{
       js: ['vue','vue-router','vuex','axios','vant','socket.io'],
       css: ['vant']
@@ -20,7 +20,7 @@ module.exports = {
   'H5/clipVideo': {
     template: 'public/h5.html',
     filename: 'clipVideo-H5.html',
-    title: '王者体育直播_NBA直播_英超直播_欧冠直播',
+    title: '',
     cdn: {
       js: ['vue','axios']
     }
